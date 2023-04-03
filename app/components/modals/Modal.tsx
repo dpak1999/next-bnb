4a3c315f84bc28c9c2411809df2a037c31297911
deepak <deepakkumardash313@gmail.com>
2023-04-04 03:05:45 +0530

    add : register modal ui

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -117,6 +117,7 @@ const Modal: FC<ModalProps> = ({
                     onClick={handleSubmit}
                   />
                 </div>
+                {footer}
               </div>
             </div>
           </div>
