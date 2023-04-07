cf642f7e1d02f24ecf6ea449dd40eff6d24faaad
deepak <deepakkumardash313@gmail.com>
2023-04-08 01:50:38 +0530

    fix hydration issues for current user

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,15 +1,15 @@
 /** @format */
 "use client";
 
-import { User } from "@prisma/client";
 import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
 import { FC } from "react";
+import { SafeUser } from "@/app/types";
 
 interface INavbar {
-  currentUser?: User | null;
+  currentUser?: SafeUser | null;
 }
 
 const Navbar: FC<INavbar> = ({ currentUser }) => {
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -8,11 +8,11 @@ import { FC, useCallback, useState } from "react";
 import MenuItem from "./MenuItem";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import useLoginModal from "@/app/hooks/useLoginModal";
-import { User } from "@prisma/client";
 import { signOut } from "next-auth/react";
+import { SafeUser } from "@/app/types";
 
 interface ICurrentUser {
-  currentUser?: User | null;
+  currentUser?: SafeUser | null;
 }
 
 const UserMenu: FC<ICurrentUser> = ({ currentUser }) => {
