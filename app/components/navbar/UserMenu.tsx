115507f033456e1e23bb900f51c456684b3354c7
deepak <deepakkumardash313@gmail.com>
2023-04-08 01:44:19 +0530

    add : signout & sign in implemetation

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -4,12 +4,18 @@
 
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
-import { useCallback, useState } from "react";
+import { FC, useCallback, useState } from "react";
 import MenuItem from "./MenuItem";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import useLoginModal from "@/app/hooks/useLoginModal";
+import { User } from "@prisma/client";
+import { signOut } from "next-auth/react";
 
-const UserMenu = () => {
+interface ICurrentUser {
+  currentUser?: User | null;
+}
+
+const UserMenu: FC<ICurrentUser> = ({ currentUser }) => {
   const registerModal = useRegisterModal();
   const loginModal = useLoginModal();
 
@@ -42,13 +48,25 @@ const UserMenu = () => {
       {isOpen && (
         <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
           <div className="flex flex-col cursor-pointer">
-            <>
-              <MenuItem onClick={() => loginModal.onOpen()} label="Login" />
-              <MenuItem
-                onClick={() => registerModal.onOpen()}
-                label="Sign Up"
-              />
-            </>
+            {currentUser ? (
+              <>
+                <MenuItem onClick={() => {}} label="My Trips" />
+                <MenuItem onClick={() => {}} label="My favorites" />
+                <MenuItem onClick={() => {}} label="My reservations" />
+                <MenuItem onClick={() => {}} label="My properties" />
+                <MenuItem onClick={() => {}} label="Next BNB my home" />
+                <hr />
+                <MenuItem onClick={() => signOut()} label="Logout" />
+              </>
+            ) : (
+              <>
+                <MenuItem onClick={() => loginModal.onOpen()} label="Login" />
+                <MenuItem
+                  onClick={() => registerModal.onOpen()}
+                  label="Sign Up"
+                />
+              </>
+            )}
           </div>
         </div>
       )}
