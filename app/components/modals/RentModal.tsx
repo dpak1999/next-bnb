8747896a00175523ad170f1d051d684715b0984a
deepak <deepakkumardash313@gmail.com>
2023-04-16 20:08:29 +0530

    add : category input & rent modal category selection

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -1,10 +1,104 @@
 "use client";
 
+import { useMemo, useState } from "react";
 import Modal from "./Modal";
 import useRentModal from "@/app/hooks/useRentModal";
+import Heading from "../Heading";
+import { categories } from "../navbar/Categories";
+import CategoryInput from "../inputs/CategoryInput";
+import { FieldValues, useForm } from "react-hook-form";
+
+enum STEPS {
+  CATEGORY = 0,
+  LOCATION = 1,
+  INFO = 2,
+  IMAGES = 3,
+  DESCRIPTION = 4,
+  PRICE = 5,
+}
 
 const RentModal = () => {
   const rentModal = useRentModal();
+
+  const [step, setStep] = useState(STEPS.CATEGORY);
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm<FieldValues>({
+    defaultValues: {
+      title: "",
+      description: "",
+      imageSrc: "",
+      category: "",
+      roomCount: 1,
+      bathroomCount: 1,
+      guestCount: 1,
+      price: 0,
+      locationValue: "",
+    },
+  });
+
+  const category = watch("category");
+
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldValidate: true,
+      shouldDirty: true,
+      shouldTouch: true,
+    });
+  };
+
+  const onBack = () => {
+    setStep((prev) => prev - 1);
+  };
+
+  const onNext = () => {
+    setStep((prev) => prev + 1);
+  };
+
+  const actionLabel = useMemo(() => {
+    if (step === STEPS.PRICE) {
+      return "Create";
+    }
+
+    return "Next";
+  }, [step]);
+
+  const secondaryActionLabel = useMemo(() => {
+    if (step === STEPS.CATEGORY) {
+      return undefined;
+    }
+
+    return "Back";
+  }, [step]);
+
+  let bodyContent = (
+    <div className="flex flex-col gap-8">
+      <Heading
+        title="Which of these best describes your place?"
+        subtitle="Pick a category"
+      />
+      <div className="grid grid-cols-1 gap-3 md:grid-cols-2 overflow-y-auto max-h-[50vh]">
+        {categories.map((item) => (
+          <div key={item.label} className="col-span-1">
+            <CategoryInput
+              icon={item.icon}
+              label={item.label}
+              selected={category === item.label}
+              onClick={(category) => {
+                setCustomValue("category", category);
+              }}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 
   return (
     <Modal
@@ -12,7 +106,10 @@ const RentModal = () => {
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
       onSubmit={rentModal.onClose}
-      actionLabel="Submit"
+      actionLabel={actionLabel}
+      secondaryActionLabel={secondaryActionLabel}
+      secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
+      body={bodyContent}
     />
   );
 };
