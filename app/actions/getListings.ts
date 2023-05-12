d2f85bbf795083afb1234a1dd956eb8ab68e3f56
deepak <deepakkumardash313@gmail.com>
2023-05-12 23:19:29 +0530

    add : getlisting action

---
+++ app/actions/getListings.ts
@@ -0,0 +1,17 @@
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth/next";
+import prisma from "@/app/libs/prismadb";
+
+export default async function getListings() {
+  try {
+    const listings = await prisma.listing.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    return listings;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
